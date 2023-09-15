eb52f759f6bd2f53c44db6ccbfb8f4ae3085bb17
Waldemar Weinert <wald.weinert@gmail.com>
2023-09-15 16:10:52 +0200

    Created appointment component with dummy dataOC

--- client/App.js
+++ client/App.js
@@ -1,8 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, View } from 'react-native';
 
-// import Header from './components/Header';
+
 import Overview from './pages/overview/Overview';
 import OnboardingNavigator from './pages/onboarding/OnboardingNavigator';
 import Login from './pages/auth/Login';
@@ -16,23 +15,12 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='onboarding' component={OnboardingNavigator} options={{headerShown: false}}/>
-
         <Stack.Screen name='overview' component={Overview} options={{title: 'Overview'}}/>
-
         <Stack.Screen name='login' component={Login} options={{title: 'Login'}} />
         <Stack.Screen name='register' component={Register} options={{title: 'Registration'}} />
 
-        {/* <View style={styles.container}></View> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
