0601cae5a75206b5582e9ef7c9e8c754e16656a4
woldemst <adlerrein@gmail.com>
2023-09-18 14:19:16 +0200

    Modified mongoose models user and order

--- server/models/Order.js
+++ server/models/Order.js
@@ -2,8 +2,18 @@ const mongoose = require("mongoose");
 
 const orderShema = new mongoose.Schema({
   name: { type: String },
-  date: { type: Number, required: true}, 
+  creator: {type: mongoose.Types.ObjectId, required: true, ref: 'User' }, // establish connecrion between two shemas
   worker: { type: String},
+  date: { type: Number, required: true}, 
+  customer: { type: String, required: true},
+  status: { type: String},
+  contact: {type: String, required: true},
+  description: {type: String},
+  streetAndHouse: {type: String},
+  zipCode: {type: Number},
+  city: {type: String},
+
 });
 
 module.exports = mongoose.model("Order", orderShema);
+
