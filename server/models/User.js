37148c4821bb52019b520f50f9e414bca06f491b
Waldemar Weinert <wald.weinert@gmail.com>
2023-09-18 14:19:16 +0200

    Modified mongoose models user and order

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,7 @@ const userShema = new mongoose.Schema({
   name: { type: String },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  orders: { type: mongoose.Types.ObjectId, required: true, ref: 'Order'} //ref: establish connection between two schemas
 });
 
 module.exports = mongoose.model("User", userShema);
