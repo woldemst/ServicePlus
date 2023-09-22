c32a61a56c5aa99aec918c08b5a62ddc05885e5f
woldemst <adlerrein@gmail.com>
2023-09-22 11:21:49 +0200

    Created functionality for creating an order in DB | Backend

--- server/models/Order.js
+++ server/models/Order.js
@@ -2,16 +2,14 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
   name: { type: String },
-  creator: {type: mongoose.Types.ObjectId, required: true, ref: 'User' }, // establish connecrion between two shemas
+  // creator: {type: mongoose.Types.ObjectId, required: true, ref: 'User' }, // establish connecrion between two shemas
+  creator: {type: String, required: true},
   worker: { type: String},
-  date: { type: Number, required: true}, 
+  date: { type: String, required: true}, 
   customer: { type: String, required: true},
   status: { type: String},
   contact: {type: String, required: true},
   description: {type: String},
-  streetAndHouse: {type: String},
-  zipCode: {type: Number},
-  city: {type: String},
 
 });
 
