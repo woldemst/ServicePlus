4d54737886002fc8c67a49bba931df8466246905
Waldemar Weinert <wald.weinert@gmail.com>
2023-09-26 15:33:22 +0200

    Created customer model in backend

---
+++ server/models/Customer.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  customerNr: { type: String, required: true },
+  organisation: { type: String, required: false },
+  street: { type: String, required: true },
+  houseNr: { type: String, required: true },
+  zip: { type: String, required: true },
+  place: { type: String, required: true },
+  phone: { type: String, required: false },
+  mobilePhone: { type: String, required: true },
+  contact: { type: String, required: true },
+  worker: { type: String, required: true },
+  nextAppointment: { type: String, required: false },
+  description: { type: String, required: false },
+
+  // creator: { type: mongoose.Types.ObjectId, required: true, ref: 'Customer'}, //ref: establish connection between two schemas
+  
+});
+
+module.exports = mongoose.model("Customer", customerSchema);
+
+
