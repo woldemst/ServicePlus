07163fd5146103a2fa6025d20724ccbd726868a2
woldemst <adlerrein@gmail.com>
2023-11-29 13:28:56 +0100

    Fixed update fuctionality in controller

--- server/routes/firm-routes.js
+++ server/routes/firm-routes.js
@@ -7,5 +7,5 @@ const firmController = require("../controllers/firm-controller")
 router.post('/register', firmController.register)
 router.patch('/update/:firmId', firmController.updateFirm)
 router.get('/profile', firmController.getFirmProfile)
-
+ 
 module.exports = router; 
