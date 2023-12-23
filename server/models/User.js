f54210ca89d288d8086038e2c2a1f773b9bd37ae
Waldemar Weinert <wald.weinert@gmail.com>
2023-12-23 19:23:24 +0100

    Fixed user functionality | register function in backend

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  orders: { type: mongoose.Types.ObjectId, required: true, ref: 'Order'} //ref: establish connection between two schemas
+  // orders: { type: mongoose.Types.ObjectId, ref: 'Order'} //ref: establish connection between two schemas
 });
 
 module.exports = mongoose.model("User", userSchema);
