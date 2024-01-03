896a564daedd272ca43640fdcc7cb075e075b7b9
woldemst <adlerrein@gmail.com>
2024-01-03 01:13:09 +0100

    Fixed userReducer logic for registering

--- client/src/reducer/userReducer.js
+++ client/src/reducer/userReducer.js
@@ -1,49 +1,97 @@
-import { UPDATE_USER_DATA, GET_USER_DATA, UPDATE_FIELD} from '../actions/userActions'
-import { SET_EMAIL, SET_PASSWORD, SET_NAME } from '../actions/userActions';
+import { UPDATE_USER_DATA, UPDATE_FIELD, CLEAR_FIELD, UPDATE_AND_VALIDATE_FIELD, TOUCH_FIELD} from '../actions/userActions'
+import { validate } from '../util/validators';
 
 const initialState = {
-    email: '',
-    password: '',
-    name: '',
+    name: {
+        value: '',
+        isValid: false,
+        isTouched: false
+    },
+    email: {
+        value: '',
+        isValid: false,
+        isTouched: false
+
+    },
+    password: {
+        value: '', 
+        isValid: false,
+        isTouched: false
+
+    },
+    isFormValid: false, 
 };
 
-const customerReducer = (state = initialState, action) => {
+const userReducer = (state = initialState, action) => {
     switch (action.type) {
-      case UPDATE_FIELD:
+        case TOUCH_FIELD:
+
             return {
                 ...state,
-                [action.payload.fieldName]: action.payload.value,
-            };
-        case UPDATE_USER_DATA:
-            return {
-                ...state,
-                userData: action.payload,
-            };
-        case GET_USER_DATA:
-            return {
-                ...state,
-                userData: action.payload,
-            };
-    
-        case SET_EMAIL:
-            return {
-                ...state,
-                email: action.payload,
-            };
-        case SET_PASSWORD:
-            return {
-                ...state,
-                password: action.payload,
-            };
-        case SET_NAME:
-            return {
-                ...state,
-                name: action.payload,
+                [action.payload]: {
+                  ...state[action.payload],
+                  isTouched: true
+                }
+            }
+        // case UPDATE_FIELD:
+        //         return {
+        //             ...state,
+        //             [action.payload.fieldName]: action.payload.value,
+        //     };
+        case UPDATE_AND_VALIDATE_FIELD:
+            const { fieldName, value, validators } = action.payload;
+            const updatedField = {
+                ...state[fieldName],
+                value,
+                isValid: validate(value, validators),
             };
 
+            const updatedState = {
+                ...state,
+                [fieldName]: updatedField,
+            };
+
+            // Check the overall form validity
+            // const isFormValid = Object.values(updatedState).every(
+            //     (field) => field.isValid
+            // );
+
+            let isFormValid = false;
+            if(updatedState.name.isValid && updatedState.email.isValid && updatedState.password.isValid){
+                isFormValid = true
+            }
+
+
+            return {
+                ...updatedState,
+                isFormValid: isFormValid
+            };
+
+        case CLEAR_FIELD:
+            return {
+                name: {
+                    value: '',
+                    isValid: false,
+                    isTouched: false
+                },
+                email: {
+                    value: '',
+                    isValid: false,
+                    isTouched: false
+
+                },
+                password: {
+                    value: '', 
+                    isValid: false,
+                    isTouched: false
+
+                },
+                isFormValid: false, 
+            };
+        
         default:
             return state;
     }
 };
 
-export default customerReducer;
+export default userReducer;
