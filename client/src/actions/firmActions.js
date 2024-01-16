b48d2511b0d4b6ba0b11a0650c74f1cbcb4595d2
Waldemar Weinert <wald.weinert@gmail.com>
2024-01-16 20:20:05 +0100

    Modified files that handle creating a new firm | modified frontned and reducers | next step firm controller in backend

--- client/src/actions/firmActions.js
+++ client/src/actions/firmActions.js
@@ -1,5 +1,7 @@
 export const UPDATE_FIRM_DATA = 'UPDATE_FIRM_DATA';
 export const GET_FIRM_DATA = 'GET_FIRM_DATA'
+export const UPDATE_AND_VALIDATE_REGISTER_FIRM_FIELD = 'UPDATE_AND_VALIDATE_REGISTER_FIRM_FIELD'
+export const UPDATE_REGISTER_FIRM_FIELD = 'UPDATE_REGISTER_FIRM_FIELD'
 
 export const updateFirmData = (firmData) => ({
   type: UPDATE_FIRM_DATA,
@@ -9,5 +11,20 @@ export const updateFirmData = (firmData) => ({
 export const getFirmData = (firmData) => ({
   type: GET_FIRM_DATA,
   payload: firmData, 
-
 })
+
+export const updateRegisterFirmField = (fieldName, value) => ({
+  type: UPDATE_REGISTER_FIRM_FIELD,
+  payload: { fieldName, value }
+})
+
+export const updateAndValidateRegisterFirmField = (fieldName, value, validators) => {
+  return {
+    type: UPDATE_AND_VALIDATE_REGISTER_FIRM_FIELD,
+    payload: {
+      fieldName,
+      value,
+      validators
+    }
+  };
+};
