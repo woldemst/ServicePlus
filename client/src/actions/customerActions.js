13db0103c38f7ada4521a80e3897205c019207ce
Waldemar Weinert <wald.weinert@gmail.com>
2024-02-06 16:08:39 +0100

    Modified customer files and customer reducer

--- client/src/actions/customerActions.js
+++ client/src/actions/customerActions.js
@@ -1,13 +1,43 @@
-export const UPDATE_CUSTOMER_DATA = 'UPDATE_CUSTOMER_DATA'
+export const UPDATE_INPUT = 'UPDATE_INPUT'
 export const GET_CUSTOMER_DATA = 'GET_CUSTOMER_DATA'
+export const UPDATE_CUSTOMER = 'UPDATE_CUSTOMER'
+export const CREATE_CUSTOMER = 'CREATE_CUSTOMER' 
+export const CLEAR_CUSTOMER_FIELD = 'CLEAR_CUSTOMER_FIELD'
 
-export const updateCustomerData = newData => ({
-    type: UPDATE_CUSTOMER_DATA,
-    payload: newData
+export const updateInput = (fieldName, value, validators) =>({
+    type: UPDATE_INPUT, 
+    payload: {
+        fieldName,
+        value,
+        validators,
+    }
 })
 
 export const getCustomerData = newData => ({
     type: GET_CUSTOMER_DATA,
     payload: newData
+})
+
+
+
+export const updateCustomer = (field, val, valid, objectId ) => ({
+    type: UPDATE_CUSTOMER,
+    payload: {
+        field,
+        val, 
+        valid,
+        objectId
+    }
+})
+
+export const createCustomer = (worker) => ({
+    type: CREATE_CUSTOMER,
+    payload: { worker }
+});
+
+
+export const clearCustomerField = (data) => ({
+    type: CLEAR_CUSTOMER_FIELD,
+    payload: data
 })
 
--- client/src/reducer/customerReducer.js
+++ client/src/reducer/customerReducer.js
@@ -1,25 +1,148 @@
-import { UPDATE_CUSTOMER_DATA, GET_CUSTOMER_DATA} from '../actions/customerActions'
+import { CREATE_CUSTOMER, GET_CUSTOMER_DATA, UPDATE_CUSTOMER, CLEAR_CUSTOMER_FIELD, UPDATE_INPUT} from '../actions/customerActions'
 
 const initialState = {
-    customerData: [],
-};
-
+    customersArray: {
+      customers: []
+    },
+    inputs: {
+      name: {
+        value: "",
+        isValid: false,
+      },
+      email: {
+        value: "",
+        isValid: false,
+      },
+      street: {
+        value: "",
+        isValid: false,
+      },
+      houseNr: {
+        value: "",
+        isValid: false,
+      },
+      zip: {
+        value: "",
+        isValid: false,
+      },
+      place: {
+        value: "",
+        isValid: false,
+      },
+      phone: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+  };
+  
 const customerReducer = (state = initialState, action) => {
     switch (action.type) {
-        case UPDATE_CUSTOMER_DATA:
-            return {
-                ...state,
-                customerData: action.payload,
-            };
+        case UPDATE_INPUT:
+          const { fieldName, value, validators } = action.payload;
+    
+          // console.log(action.payload);
+          const updatedField = {
+            ...state.inputs[fieldName],
+            value,
+            isValid: validate(value, validators)
+          };
+          const updatedInputs = {
+            ...state.inputs,
+            [fieldName]: updatedField,
+          };
+    
+          let isFormValid = Object.values(updatedInputs).every((field) => field.isValid);
+    
+          return {
+            ...state,
+            inputs: updatedInputs,
+            isFormValid,
+          };
+        case UPDATE_CUSTOMER:
+          const { field, val, objectId } = action.payload;
+    
+          const updatedWorker = {
+            ...state.customersArray.customers.find(worker => worker._id === objectId),
+            [field]: val
+          };
+    
+          // console.log(updatedWorker);
+          const updatedWorkersArray = state.customersArray.customers.map(worker => {
+            if (worker._id === objectId) {
+              return updatedWorker;
+            }
+            return worker;
+          });
+    
+          return {
+            ...state,
+            customersArray: {
+              ...state.customersArray,
+              customers: updatedWorkersArray,
+            },
+          };
         case GET_CUSTOMER_DATA:
-            return {
-                ...state,
-                customerData: action.payload,
-            };
-
+          return {
+            ...state,
+            customersArray: action.payload,
+          };
+        case CREATE_CUSTOMER:
+          const newCustomer = action.payload.worker;
+          // console.log(action.payload);
+          return {
+            ...state,
+            customersArray: {
+              ...state.customersArray,
+              customers: [...state.customersArray.customers, newCustomer],
+            },
+          };
+        case CLEAR_CUSTOMER_FIELD:
+          return {
+            ...state,
+            inputs: {
+              name: {
+                value: "",
+                isValid: false,
+              },
+              email: {
+                value: "",
+                isValid: false,
+              },
+              street: {
+                value: "",
+                isValid: false,
+              },
+              houseNr: {
+                value: "",
+                isValid: false,
+              },
+              zip: {
+                value: "",
+                isValid: false,
+              },
+              place: {
+                value: "",
+                isValid: false,
+              },
+              phone: {
+                value: "",
+                isValid: false,
+              },
+              description: {
+                value: "",
+                isValid: false,
+              },
+            },
+            isFormValid: false,
+          };
         default:
-            return state;
-    }
+          return state;
+      }
 };
 
 export default customerReducer;
