d284b35856793edfc961945ca81f279f2101a96e
Waldemar Weinert <wald.weinert@gmail.com>
2024-02-15 18:48:51 +0100

    Created backend logic for creating a new order and update the object's firm array in db

--- server/models/Firm.js
+++ server/models/Firm.js
@@ -2,6 +2,10 @@
 const mongoose = require("mongoose");
 
 const firmSchema = new mongoose.Schema({
+  userId: {type: mongoose.Types.ObjectId, required: false, ref: 'User'},
+  customers: [{type: mongoose.Types.ObjectId, required: false, ref: 'Customer'}],
+  workers: [{type: mongoose.Types.ObjectId, required: false, ref: 'Worker'}],
+  orders: [{type: mongoose.Types.ObjectId, required: false, ref: 'Order'}],
   ownerName: { type: String, required: false},
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
@@ -11,11 +15,6 @@ const firmSchema = new mongoose.Schema({
   place: { type: String, required: true }, 
   phone: { type: String, required: true },
   website: { type: String, required: true }, 
-  orders: [{type: mongoose.Types.ObjectId, required: false, ref: 'Order'}],
-  customers: [{type: mongoose.Types.ObjectId, required: false, ref: 'Customer'}],
-  workers: [{type: mongoose.Types.ObjectId, required: false, ref: 'Worker'}],
-  userId: {type: mongoose.Types.ObjectId, required: false, ref: 'User'}
-
 });
 
 module.exports = mongoose.model("Firm", firmSchema);
--- server/routes/order-routes.js
+++ server/routes/order-routes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 const orderController = require('../controllers/order-controller')
 
 router.get('/:firmId/all', orderController.getAllOrdersByFirmId)
-router.post('/create', orderController.createOrder)
+router.post('/:firmId/new', orderController.createOrder)
 router.get('/:orderId', orderController.getOrderById)
 
 module.exports = router;
