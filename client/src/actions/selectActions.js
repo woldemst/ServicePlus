edbc5bfdd23ff80babff53b68072f9a49f315058
woldemst <adlerrein@gmail.com>
2024-02-29 16:56:23 +0100

    Adjusted a reusable input and userReducer and used in Login screen

--- client/src/actions/selectActions.js
+++ client/src/actions/selectActions.js
@@ -1,13 +1,14 @@
 export const SET_SELECT_TOUCHED = 'SET_SELECT_TOUCHED'
 export const SET_SELECT = 'SET_SELECT'
 
-export const setSelect = (field, val, valid) => {
+export const setSelect = (field, val, valid, objectId = null) => {
   return {
     type: SET_SELECT,
     payload: {
       field,
       val,
-      valid
+      valid,
+      objectId
     },
   };
 };
