fe36eb7b9b2d94c70506ce5b693cc2ae59590c2d
Waldemar Weinert <wald.weinert@gmail.com>
2024-03-03 18:54:10 +0100

    Fixed the route and controller for getting workers as option

--- server/routes/order-routes.js
+++ server/routes/order-routes.js
@@ -8,4 +8,7 @@ router.get('/:firmId/all', orderController.getAllOrdersByFirmId)
 router.post('/:firmId/new', orderController.createOrder)
 router.get('/:orderId', orderController.getOrderById)
 
+// not stable 
+router.get('/worker-options/:firmId', orderController.getAllWorkersAsOptions)
+
 module.exports = router;
