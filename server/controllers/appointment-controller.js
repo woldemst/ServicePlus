817f42f137043bc5bdbc72c54bab19460eefc9b4
Waldemar Weinert <wald.weinert@gmail.com>
2024-03-29 16:47:35 +0100

    Created an endpoint for getting a list of customers

--- server/controllers/appointment-controller.js
+++ server/controllers/appointment-controller.js
@@ -3,92 +3,123 @@ const Appointment = require("../models/Appointment")
 const Customer = require('../models/Customer')
 
 const getAllAppointments = async (req, res, next) => {
-    try {
-        const appointments = await Appointment.find()
-        res.json(appointments)
-    } catch (err) {
-        const error = new HttpError(
-            "Fetch the appointments failed, please try again later.",
-            500
-        )
-        next(error)
-    }
+  try {
+    const appointments = await Appointment.find()
+    res.json(appointments)
+  } catch (err) {
+    const error = new HttpError(
+      "Fetch the appointments failed, please try again later.",
+      500
+    )
+    next(error)
+  }
 }
 
 const createAppointment = async (req, res, next) => {
 
-    const {
-        // firmId,
-        // creator,
-        // status,
+  const {
+    // firmId,
+    // creator,
+    // status,
 
-        order,
-        worker,
-        customer,
-        name,
-        date,
-        startTime,
-        finishTime,
-        description,
-    } = req.body;
+    order,
+    worker,
+    customer,
+    name,
+    date,
+    startTime,
+    finishTime,
+    description,
+  } = req.body;
 
 
-    const createdAppointment = new Appointment({
-        // firmId: firmId,
-        // creator: creator, // auth.userId in frontend 
-        // status: status,
-        order: order,
-        worker: worker,
-        customer: customer,
-        name: name,
-        date: date,
-        startTime: startTime,
-        finishTime: finishTime,
-        description: description,
-    });
-    try {
-        await createdAppointment.save()
-        res.status(201).json({ appointment: createdAppointment.toObject({ getters: true }) }); // Send a response indicating success
+  const createdAppointment = new Appointment({
+    // firmId: firmId,
+    // creator: creator, // auth.userId in frontend 
+    // status: status,
+    order: order,
+    worker: worker,
+    customer: customer,
+    name: name,
+    date: date,
+    startTime: startTime,
+    finishTime: finishTime,
+    description: description,
+  });
+  try {
+    await createdAppointment.save()
+    res.status(201).json({ appointment: createdAppointment.toObject({ getters: true }) }); // Send a response indicating success
 
-    } catch (err) {
-        const error = new HttpError(
-            "Something went wrong, could not create an appointment.",
-            500
-        );
-        return next(error);
-    }
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not create an appointment.",
+      500
+    );
+    return next(error);
+  }
 }
 
 const getAllContactsAsOptionsByFirmId = async (req, res, next) => {
-    const firmId = req.params.firmId;
-    
-    try {
-      const contacts = await Customer.find({ firmId: firmId })
-  
-      if (!contacts || contacts.length === 0) {
-        const error = new HttpError(
-          'Could not find contacts for the provided firm id.',
-          404
-        );
-        return next(error);
-      }
-  
-      const contactList = contacts.map(contact => ({
-        id: contact._id,
-        name: contact.contact
-      }))
-  
-      res.json({ contacts: contactList })
-    } catch (err) {
+  const firmId = req.params.firmId;
+
+  try {
+    const contacts = await Customer.find({ firmId: firmId })
+
+    if (!contacts || contacts.length === 0) {
       const error = new HttpError(
-        'Fetching contacts failed, please try again later.',
-        500
+        'Could not find contacts for the provided firm id.',
+        404
       );
       return next(error);
     }
+
+    const contactList = contacts.map(contact => ({
+      id: contact._id,
+      name: contact.contact
+    }))
+
+    res.json({ contacts: contactList })
+  } catch (err) {
+    const error = new HttpError(
+      'Fetching contacts failed, please try again later.',
+      500
+    );
+    return next(error);
   }
+}
+
+const getAllCustomersAsOptionsBiFirmId = async (req, res, next) => {
+  const firmId = req.params.firmId
+
+  try {
+    const customers = await Customer.find({ firmId: firmId });
+
+    if (!customers || customers.length === 0) {
+      const error = new HttpError(
+        'Could not find customers for the provided firm id.',
+        404
+      );
+      return next(error);
+    }
+
+    const customerList = customers.map(customer => ({
+      id: customer._id,
+      name: customer.name
+    }))
+
+    res.json({ customers: customerList })
+  } catch (err) {
+    const error = new HttpError(
+      'Fetching customers failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+}
+
 
 
 exports.getAllAppointments = getAllAppointments;
 exports.createAppointment = createAppointment;
 exports.getAllContactsAsOptionsByFirmId = getAllContactsAsOptionsByFirmId;
+exports.getAllCustomersAsOptionsBiFirmId = getAllCustomersAsOptionsBiFirmId; 
--- server/routes/appointment-routes.js
+++ server/routes/appointment-routes.js
@@ -7,4 +7,6 @@ const appointmentController = require('../controllers/appointment-controller')
 router.get('/all', appointmentController.getAllAppointments)
 router.post('/new', appointmentController.createAppointment)
 router.get('/contact-options/:firmId', appointmentController.getAllContactsAsOptionsByFirmId)
+router.get('/customer-options/:firmId', appointmentController.getAllCustomersAsOptionsBiFirmId)
+
 module.exports = router;
