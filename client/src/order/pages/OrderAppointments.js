9476f9c48c8967698fdd6b62ae1246ec3a30e83d
Waldemar Weinert <wald.weinert@gmail.com>
2024-04-06 18:31:52 +0200

    Created listing of appointments in OrderMain

--- client/src/order/pages/OrderAppointments.js
+++ client/src/order/pages/OrderAppointments.js
@@ -1,13 +1,23 @@
-import { View } from "react-native";
-import { Text } from "react-native-elements";
+import { View, StyleSheet } from "react-native";
+import OrderList from "../components/OrderList"
+import AppointmentList from "../../appointment/components/AppointmentList";
 
 
 const OrderAppointments = () => {
     return <>
-        <View>
-            <Text>Appointments</Text>
+        <View style={styles.appointmentListing} >
+            <AppointmentList />
         </View>
     </>
 }
 
-export default OrderAppointments;
+const styles = StyleSheet.create({
+    appointmentListing: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingVertical: 32,
+        paddingHorizontal: 16
+    }
+})
+
+export default OrderAppointments; 
