ab4ee7311fb7f118aef283f1b636adea54b2b480
Waldemar Weinert <wald.weinert@gmail.com>
2024-04-20 19:54:10 +0200

    Created  a functionality of appointment's status

--- client/src/appointment/pages/AppointmentInfo.js
+++ client/src/appointment/pages/AppointmentInfo.js
@@ -1,26 +1,36 @@
 import { View, StyleSheet, Image, Text, TouchableOpacity } from "react-native";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { refershData } from "../../actions/utilActions";
 import { Alert } from "react-native";
 
+import Button from "../../shared/UIElements/Button";
+
+
 
 const AppointmentInfo = props => {
+    const [edit, setEdit] = useState(false);
+    
     const dispatch = useDispatch()
-    const [status, setStatus] = useState(props.status || 1);
-    const [showOptions, setShowOptions] = useState(false);
+    
+    const appointments = useSelector(state => state.appointment.appointmentsArray.appointments)
+    const appointmentItem = appointments.find(appointment => appointment._id == props.id)
+    const [activeStatus, setActiveStatus] = useState(props.status);
 
+    // console.log(appointmentItem.status);
 
-    const statusStrings = {
-        1: "new",
-        2: "in progress",
-        3: "completed",
-        4: "canceled"
-    };
+    
+    useEffect(() => {
+        setActiveStatus(appointmentItem.status)
+        if (activeStatus !== appointmentItem.status) {
+            setActiveStatus(activeStatus)
+        }
+    }, [activeStatus, edit])
+
 
     const handleStatusChange = async newStatus => {
-        setStatus(newStatus);
+        setActiveStatus(newStatus);
 
         Alert.alert(
             'Changing Confirmation',
@@ -31,12 +41,12 @@ const AppointmentInfo = props => {
                     style: 'cancel',
                 },
                 {
-                    text: "Delete",
+                    text: "Change",
                     onPress: async () => {
                         try {
                             const response = await axios.post(`http://localhost:8000/api/appointments/${props.id}/statuschange`, { newStatus: newStatus })
                             dispatch(refershData())
-                            setShowOptions(false);
+                            setEdit(false)
 
                         } catch (err) {
                             console.log("Error if changing a status of an appointments", err);
@@ -49,9 +59,6 @@ const AppointmentInfo = props => {
 
     };
 
-    const toggleOptions = () => {
-        setShowOptions(!showOptions);
-    };
 
     return <>
         <View style={styles.container}>
@@ -80,68 +87,70 @@ const AppointmentInfo = props => {
                     <Text style={[styles.boldCell, styles.cell]}>Adresse:</Text>
                     <Text style={[styles.thinCell, styles.cell]}>{props.o_street} {props.o_houseNr} {props.o_zip} {props.o_place}</Text>
                 </View>
-                <View style={styles.row}>
-                    <Text style={[styles.boldCell, styles.cell]}>Status:</Text>
 
-                    <View style={[styles.statusContainer]}>
+                <View style={[styles.statusContainer]}>
+                    <TouchableOpacity
+                        style={[
+                            {
+                                borderTopLeftRadius: 4,
+                                borderBottomLeftRadius: 4
+                            },
+                            styles.statusButton,
+                            edit ? [activeStatus == 2 ? styles.inProgress : styles.disabledButton] : [activeStatus == 2 ? styles.currentOfflineStatus : styles.disabledButton],
+                        ]}
+                        onPress={() => handleStatusChange(2)}
+                        disabled={!edit}
+                    >
+                        <Text style={styles.statusButtonText}>In Progress</Text>
+                    </TouchableOpacity>
 
-                        <View style={[
-                            styles.statusLine,
-                            status === '1' ? styles.notStarted : null,
-                            status === '2' ? styles.inProgress : null,
-                            status === '3' ? styles.completed : null,
-                            status === '4' ? styles.canceled : null
-                        ]}></View>
+                    <TouchableOpacity
+                        style={[
+                            styles.statusButton,
+                            edit ? [activeStatus == 3 ? styles.completed : styles.disabledButton] : [activeStatus == 3 ? styles.currentOfflineStatus : styles.disabledButton],
+                        ]}
+                        onPress={() => handleStatusChange(3)}
+                        disabled={!edit}
+                    >
+                        <Text style={styles.statusButtonText}>Completed</Text>
+                    </TouchableOpacity>
 
-                        <Text style={styles.statusText}>
-                            {(() => {
-                                switch (status) {
-                                    case '1':
-                                        return 'Not started';
-                                    case '2':
-                                        return 'In progress';
-                                    case '3':
-                                        return 'Completed';
-                                    case '4':
-                                        return 'Canceled';
-                                    default:
-                                        return '';
-                                }
-                            })()}
-                        </Text>
+                    <TouchableOpacity
+                        style={[
+                            {
+                                borderTopRightRadius: 4,
+                                borderBottomRightRadius: 4
+                            },
+                            styles.statusButton,
+                            edit ? [activeStatus == 4 ? styles.canceled : styles.disabledButton] : [activeStatus == 4 ? styles.currentOfflineStatus : styles.disabledButton],
+                        ]}
+                        onPress={() => handleStatusChange(4)}
+                        disabled={!edit}>
+                        <Text style={styles.statusButtonText}>Canceled</Text>
+                    </TouchableOpacity>
 
-                        <View>
-                            <View style={[
-                                styles.thinCell,
-                                styles.cell,
-                                status === '1' ? styles.notStarted : null,
-                                status === '2' ? styles.inProgress : null,
-                                status === '3' ? styles.completed : null,
-                                status === '4' ? styles.canceled : null
-
-                            ]}></View>
-                            <TouchableOpacity onPress={toggleOptions} style={styles.statusChangeButton}>
-                                {/* <Text style={styles.changeButtonText}>Change</Text> */}
-                                <Image source={require('../../../assets/buttons/edit.png')} />
-                            </TouchableOpacity>
-
-                        </View>
-                    </View>
                 </View>
-                {showOptions && (
-                    <View style={styles.btnContainer}>
-                        <TouchableOpacity onPress={() => handleStatusChange("2")}>
-                            <Text style={[styles.statusButton, styles.inProgress]}>In Progress</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => handleStatusChange("3")}>
-                            <Text style={[styles.statusButton, styles.completed]}>Completed</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => handleStatusChange("4")}>
-                            <Text style={[styles.statusButton, styles.canceled]}>Canceled</Text>
-                        </TouchableOpacity>
-                    </View>
+
+            </View>
+
+            <View style={styles.btnContainer}>
+                {edit ? (
+                    <Button
+                        style={[styles.editButton, styles.button]}
+                        buttonText={styles.editButtonText}
+                        onPress={() => setEdit(false)}
+                        title={'Abbrechen'}
+                    />
+                ) : (
+                    <Button
+                        style={[styles.invalideButton, styles.button]}
+                        buttonText={styles.editButtonText}
+                        onPress={() => setEdit(true)}
+                        title={'Ändern'}
+                    />
                 )}
             </View>
+
         </View>
     </>
 }
@@ -152,20 +161,12 @@ const styles = StyleSheet.create({
         paddingTop: 16,
         paddingBottom: 16
     },
-    table: {
-
-    },
     row: {
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-between',
         marginBottom: 10,
         // borderColor: 'green',
-        // borderWidth: 2
-    },
-    cell: {
-
-        // borderColor: 'red',
         // borderWidth: 2
     },
     boldCell: {
@@ -177,83 +178,104 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: '400'
     },
+
     // status 
     statusContainer: {
         flexDirection: 'row',
-        justifyContent: 'flex-start',
-        width: '60%',
-
-        // borderWidth: 2,
-        // borderColor: 'red'
-    },
-    inProgress: {
-        backgroundColor: 'yellow',
-        color: 'black'
-    },
-    completed: {
-        backgroundColor: 'green',
-        color: 'white'
-    },
-    canceled: {
-        backgroundColor: 'red',
-        color: 'white'
-    },
-    statusChangeButton: {
-        backgroundColor: '#e0e0e0',
-        borderRadius: 4,
-        padding: 5,
-    },
-    changeButtonText: {
-        color: 'black',
-        fontWeight: 'bold',
-    },
-    statusLine: {
-        width: 24,
-        height: 24,
-        borderRadius: 50
-    },
-    notStarted: {
-        backgroundColor: '#808080',
-        color: 'white'
-    },
-    inProgress: {
-        backgroundColor: '#1769FF',
-        color: 'white'
-    },
-    completed: {
-        backgroundColor: '#7A9B76',
-        color: 'white'
-    },
-    canceled: {
-        backgroundColor: '#DB504A',
-        color: 'white'
-    },
-
-    statusText: {
-        paddingHorizontal: 6,
-        paddingVertical: 2
-    },
-    statusChangeButton: {
-        backgroundColor: '#e0e0e0',
-        borderRadius: 4,
-        padding: 5,
-    },
-    changeButtonText: {
-        color: 'black',
-        fontWeight: 'bold',
-    },
-    btnContainer: {
-        flexDirection: 'row',
         justifyContent: 'space-between',
-        // borderWidth: 2,
+        marginBottom: 16,
+        marginTop: 8,
+        // borderWidth: 1,
         // borderColor: 'red'
     },
     statusButton: {
-        padding: 8,
-        borderRadius: 4,
+        flex: 1,
+        paddingVertical: 8,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+        borderWidth: 1,
+
+    },
+    statusButtonText: {
         fontWeight: 'bold',
-        textAlign: 'center',
-        width: 110
+        color: 'white',
+
+    },
+    disabledButton: {
+        backgroundColor: 'grey',
+        color: 'white',
+        opacity: 0.7,
+        borderColor: '#222',
+        borderWidth: 1,
+
+
+    },
+    notStarted: {
+        backgroundColor: '#eee',
+        color: 'white',
+        borderWidth: 1,
+    },
+    inProgress: {
+        backgroundColor: '#1769FF',
+        color: 'white',
+        borderColor: '#1769FF',
+
+    },
+    completed: {
+        backgroundColor: '#7A9B76',
+        color: 'white',
+        borderColor: '#7A9B76',
+        borderRightWidth: 0,
+        borderLeftWidth: 0
+    },
+    canceled: {
+        backgroundColor: '#DB504A',
+        borderColor: '#DB504A',
+        color: 'white',
+    },
+    currentOfflineStatus: {
+        backgroundColor: '#222',
+        borderColor: '#222',
+        opacity: 0.7,
+    },
+    // button 
+    btnContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+
+    button: {
+        // height: 53,    
+        padding: 16,
+        width: '100%',
+        borderRadius: 4,
+        justifyContent: "center",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    cancelBtnText: {
+        fontSize: 18,
+        color: "#7A9B76",
+        fontWeight: "700",
+    },
+    editButton: {
+        backgroundColor: "#7A9B76",
+    },
+    editButtonText: {
+        fontSize: 18,
+        color: "#fff",
+        fontWeight: "700",
+    },
+    invalideButton: {
+        height: 53,
+        width: '40%',
+        backgroundColor: 'gray',
+        borderRadius: 5,
+        justifyContent: 'center',
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: 'center'
     },
 })
 
