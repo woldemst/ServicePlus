8cd0c55764c44955cdfd5e0779cbd2ce4ad3d0c3
Waldemar Weinert <wald.weinert@gmail.com>
2024-04-23 11:04:38 +0200

    Remade a logic of the reusable input and select components | now they work usinc a logic of the useState. There is no redux anymore

--- client/src/actions/inputActions.js
+++ client/src/actions/inputActions.js
@@ -2,6 +2,7 @@ export const SET_TOUCHED = 'SET_TOUCHED'
 export const SET_INPUT = 'SET_INPUT'
 export const SET_INITIAL_INPUT_DATA = 'SET_INITIAL_INPUT_DATA'
 export const ADD_TO_INITIAL_DATA = 'ADD_TO_INITIAL_DATA'
+export const ERASE_INPUT_DATA = 'ERASE_INPUT_DATA'
 
 
 export const setInput = (fieldName, value, validators, objectId = null, reducerKey = null) => {
@@ -20,6 +21,10 @@ export const setInput = (fieldName, value, validators, objectId = null, reducerK
 export const setInitialInputData = data => ({
   type: SET_INITIAL_INPUT_DATA,
   payload: data
+})
+
+export const eraseInpuData = () => ({
+  type: ERASE_INPUT_DATA,
 })
 
 export const addToInitialData = data => ({
--- client/src/shared/UIElements/Select.js
+++ client/src/shared/UIElements/Select.js
@@ -2,26 +2,16 @@ import { StyleSheet } from "react-native";
 import { SelectList } from "react-native-dropdown-select-list";
 import { useState, useEffect } from "react";
 import { validate } from "../../util/validators";
-import { useDispatch, useSelector } from "react-redux";
-import { setSelect } from "../../actions/selectActions";
 
 
 const Select = props => {
-  const dispatch = useDispatch()
-  const [touched, setTouched] = useState(false)
+  // const [touched, setTouched] = useState(false)
   const [isValid, setIsValid] = useState(true)
-  const fetchedData = useSelector((state) => state[props.reducerKey]);
 
-  // useEffect(() => {
-  //   if (touched) {
-  //     setIsValid(validate(props.value, props.validators))
-  //   }
-  // }, [touched])
 
   const handleChange = val => {
-    setIsValid(validate(val, props.validators))
-    dispatch(setSelect(props.fieldName, val, props.validators, props.objectId, props.reducerKey))
-
+    props.onValueChange(val);
+    setIsValid(validate(val, props.validators));
   }
 
   return <>
