50bad51dabaab233f3bea0006965fffcf35978fe
Waldemar Weinert <wald.weinert@gmail.com>
2024-04-27 15:28:09 +0200

    Modified Select component and deleted the old

--- client/src/shared/UIElements/Select.js
+++ client/src/shared/UIElements/Select.js
@@ -1,68 +1,83 @@
+import { Dropdown } from 'react-native-element-dropdown';
 import { StyleSheet } from "react-native";
-import { SelectList } from "react-native-dropdown-select-list";
 import { useState, useEffect } from "react";
+
 import { validate } from "../../util/validators";
 
-
 const Select = props => {
-  // const [touched, setTouched] = useState(false)
-  const [isValid, setIsValid] = useState(true)
+    const [touched, setTouched] = useState(false)
+    const [isFocus, setIsFocus] = useState(false);
+    const [isValid, setIsValid] = useState(true)
+    const [value, setValue] = useState(null)
 
+    const [selectedValue, setSelectedValue] = useState(props.initialSelectedValue); // Set initial selected value
 
-  const handleChange = val => {
-    const selectedOption = props.data.find(option => option.value === val);
-    props.onValueChange(selectedOption); 
-    setIsValid(validate(val, props.validators));
-  }
+    const handleChange = item => {
+        props.onValueChange(item.value)
+        // setIsFocus(false);
+        setIsValid(validate(item.value, props.validators));
+        setSelectedValue(item.value)
+        setValue(item.value)
+    }
 
-  return <>
-    <SelectList
-      save='value'
-      id={props.id}
-      data={props.data}
-      search={props.search}
-      setSelected={handleChange}
-      onFocus={() => setTouched(true)}
-      placeholder={props.placeholder}
-      boxStyles={!isValid ? styles.selectInvalid : styles.select}
-      inputStyles={!isValid ? styles.placeholderTextInvalid : styles.placeholderText}
-      dropdownStyles={styles.dropdown}
-      defaultOption={props.defaultOption}
-      dropdownShown={false}
-      disabledItemStyles={!props.disabled ? styles.selectInvalid : styles.select}
-    />
-
-  </>
+    return <>
+        <Dropdown
+            // placeholderStyle={styles.placeholderStyle}
+            selectedTextStyle={styles.selectedTextStyle}
+            inputSearchStyle={styles.inputSearchStyle}
+            style={[props.disable && styles.disaabled, !isValid ? styles.selectInvalid : styles.select]}
+            iconStyle={styles.iconStyle}
+            data={props.data}
+            search={props.search}
+            maxHeight={300}
+            labelField="label"
+            valueField="value"
+            placeholder={selectedValue ? selectedValue : 'Auswählen'}
+            visibleSelectedItem={true}
+            value={value}
+            disable={props.disable}
+            // onFocus={() => setIsFocus(true)}
+            // onBlur={() => setIsFocus(false)}
+            onChange={handleChange}
+        />
+    </>
 }
 
 const styles = StyleSheet.create({
-  select: {
-    height: 50,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginTop: 14,
-    alignItems: 'center',
-    borderRadius: 6
-  },
-  selectInvalid: {
-    borderColor: 'red',
-    height: 50,
-    color: 'red',
-    borderWidth: 1,
-    marginTop: 14,
-    alignItems: 'center',
-    borderRadius: 6
-  },
-  placeholderText: {
-    color: "#000",
-  },
-  placeholderTextInvalid: {
-    color: "red",
+    select: {
+        height: 50,
+        borderColor: "#e0e0e0",
+        borderWidth: 1,
+        marginTop: 8,
+        alignItems: 'center',
+        borderRadius: 6,
+        paddingHorizontal: 8
+    },
+    selectInvalid: {
+        borderColor: 'red',
+        height: 50,
+        color: 'red',
+        borderWidth: 1,
+        marginTop: 14,
+        alignItems: 'center',
+        borderRadius: 6
+    },
+    disaabled: {
+        backgroundColor: '#eee'
+    },
+    placeholderText: {
+        color: "#000",
+    },
+    placeholderTextInvalid: {
+        color: "red",
 
-  },
-  dropdown: {
-    borderRadius: 6
-  }
+    },
+    dropdown: {
+        borderRadius: 6
+    },
+    selectedTextStyle: {
+        // color: 'red'
+    }
 
 })
 
