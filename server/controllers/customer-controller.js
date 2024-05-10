5582f2c78d89a0b17b6534b1d3e11c38229bba4d
woldemst <adlerrein@gmail.com>
2024-05-10 13:16:50 +0200

    Added the functionality for deleting a customer in backend

--- server/controllers/customer-controller.js
+++ server/controllers/customer-controller.js
@@ -3,76 +3,76 @@ const Customer = require('../models/Customer')
 const Firm = require('../models/Firm')
 
 const getAllCustomersByFirmId = async (req, res, next) => {
-  const firmId = req.params.firmId; 
+  const firmId = req.params.firmId;
 
-    try {
-        const customers = await Customer.find({ firmId: firmId });
+  try {
+    const customers = await Customer.find({ firmId: firmId });
 
-        if (!customers || customers.length === 0) {
-          const error = new HttpError(
-              'Could not find customers for the provided firm id.',
-              404
-            );
-            return next(error);
-        }
-      res.json({customers: customers.map(customer => customer.toObject({getters: true}))})
-    } catch (err) {
-        const error = new HttpError(
-            "Fetch the customers failed, please try again later.",
-            500
-        )
-        next(error)
-    }
-}
-
-const getCustomerById = async (req, res, next) => {
-    const customerId = req.params.customerId;
-    
-    let customer;
-  
-    try {
-      customer = await Customer.findById(customerId);
-    } catch (err) {
+    if (!customers || customers.length === 0) {
       const error = new HttpError(
-        "Something went wrong, could not find a customer.",
-        500
+        'Could not find customers for the provided firm id.',
+        404
       );
       return next(error);
     }
-  
-    if (!customer) {
-      const error = new HttpError(
-          'Could not find customer for the provided id.',
-          404
-        );
-        return next(error)
-    }
-  
-    res.json({ customer: customer.toObject({getters: true}) })
+    res.json({ customers: customers.map(customer => customer.toObject({ getters: true })) })
+  } catch (err) {
+    const error = new HttpError(
+      "Fetch the customers failed, please try again later.",
+      500
+    )
+    next(error)
+  }
+}
+
+const getCustomerById = async (req, res, next) => {
+  const customerId = req.params.customerId;
+
+  let customer;
+
+  try {
+    customer = await Customer.findById(customerId);
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not find a customer.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!customer) {
+    const error = new HttpError(
+      'Could not find customer for the provided id.',
+      404
+    );
+    return next(error)
+  }
+
+  res.json({ customer: customer.toObject({ getters: true }) })
 };
-  
+
 const updateCustomerById = async (req, res, next) => {
   const customerId = req.params.customerId
- 
+
   const {
-    name, 
-    email, 
-    customerNr, 
-    organisation, 
-    street, 
-    houseNr, 
-    zip, 
-    place, 
-    phone, 
-    mobilePhone, 
-    contact, 
-    worker, 
-    nextAppointment, 
-    website,  
-    description 
+    name,
+    email,
+    customerNr,
+    organisation,
+    street,
+    houseNr,
+    zip,
+    place,
+    phone,
+    mobilePhone,
+    contact,
+    worker,
+    nextAppointment,
+    website,
+    description
   } = req.body
 
-  let updatedCustomer; 
+  let updatedCustomer;
 
   try {
     updatedCustomer = await Customer.findById(customerId)
@@ -83,23 +83,23 @@ const updateCustomerById = async (req, res, next) => {
     }
 
     updatedCustomer.name = name,
-    updatedCustomer.email = email,
-    updatedCustomer.customerNr = customerNr,
-    updatedCustomer.organisation = organisation,
-    updatedCustomer.nextAppointment = nextAppointment,
-    updatedCustomer.mobilePhone = mobilePhone,
-    updatedCustomer.contact = contact,
-    updatedCustomer.worker = worker,
-    updatedCustomer.description = description,
-    updatedCustomer.street = street,
-    updatedCustomer.houseNr = houseNr,
-    updatedCustomer.zip = zip,
-    updatedCustomer.place = place,
-    updatedCustomer.phone = phone,
-    updatedCustomer.website = website,
+      updatedCustomer.email = email,
+      updatedCustomer.customerNr = customerNr,
+      updatedCustomer.organisation = organisation,
+      updatedCustomer.nextAppointment = nextAppointment,
+      updatedCustomer.mobilePhone = mobilePhone,
+      updatedCustomer.contact = contact,
+      updatedCustomer.worker = worker,
+      updatedCustomer.description = description,
+      updatedCustomer.street = street,
+      updatedCustomer.houseNr = houseNr,
+      updatedCustomer.zip = zip,
+      updatedCustomer.place = place,
+      updatedCustomer.phone = phone,
+      updatedCustomer.website = website,
 
-    await updatedCustomer.save();
-    
+      await updatedCustomer.save();
+
     res
       .status(200)
       .json({ customer: updatedCustomer.toObject({ getters: true }) });
@@ -117,39 +117,39 @@ const createCustomer = async (req, res, next) => {
   // const firmId = req.params.firmId; 
 
   const {
-     firmId,
-     // customerNr, 
-     // organisation, 
-     // worker, 
-     name, 
-     email, 
-     street, 
-     houseNr, 
-     zip, 
-     place, 
-     phone, 
-     contact, 
-     nextAppointment, 
-     website,  
-     description 
+    firmId,
+    // customerNr, 
+    // organisation, 
+    // worker, 
+    name,
+    email,
+    street,
+    houseNr,
+    zip,
+    place,
+    phone,
+    contact,
+    nextAppointment,
+    website,
+    description
   } = req.body
-  
+
 
   const createCustomer = new Customer({
     firmId: firmId,
     // customerNr: customerNr, 
     // organisation: organisation, 
     // worker: worker, 
-    name: name, 
-    email: email, 
-    street: street, 
-    houseNr: houseNr, 
-    zip: zip, 
-    place: place, 
-    phone: phone, 
-    contact: contact, 
-    nextAppointment: nextAppointment, 
-    website: website,  
+    name: name,
+    email: email,
+    street: street,
+    houseNr: houseNr,
+    zip: zip,
+    place: place,
+    phone: phone,
+    contact: contact,
+    nextAppointment: nextAppointment,
+    website: website,
     description: description
   })
 
@@ -159,8 +159,8 @@ const createCustomer = async (req, res, next) => {
     const error = new HttpError(
       "Something went wrong, could not create profile.",
       500
-      );
-      return next(error);
+    );
+    return next(error);
   }
 
   await Firm.updateOne(
@@ -175,20 +175,22 @@ const createCustomer = async (req, res, next) => {
 
 // delete functionality 
 const deleteCustomerById = async (req, res, next) => {
-  const orderId = req.params.orderId
-
+  const { customerId, firmId } = req.params
   try {
-    // Delete the appointment
-    const deletedCustomer = await Customer.deleteOne({ _id: orderId });
+    // First, find the customer to check if it exists
+    const customer = await Customer.findById(customerId);
 
-    // Check if the appointment exists
-    if (!deletedCustomer) {
+    // If customer does not exist, return an error
+    if (!customer) {
       return res.status(404).json({ message: 'Customer not found' });
     }
 
+    // If customer exists, proceed to delete
+    await Customer.deleteOne({ _id: customerId });
+
     await Firm.findOneAndUpdate(
-      { _id: orderId },
-      { $pull: { customers: orderId } }
+      { _id: firmId },
+      { $pull: { customers: customerId } }
     );
 
     res.status(200).json({ message: 'Customer was deleted successfully' });
@@ -201,7 +203,8 @@ const deleteCustomerById = async (req, res, next) => {
   }
 }
 
-exports.getAllCustomersByFirmId = getAllCustomersByFirmId; 
+exports.getAllCustomersByFirmId = getAllCustomersByFirmId;
 exports.getCustomerById = getCustomerById;
 exports.updateCustomerById = updateCustomerById;
 exports.createCustomer = createCustomer;
+exports.deleteCustomerById = deleteCustomerById;
