9448812ef0e7ec1959e2aca3dbae1217c5a99208
Waldemar Weinert <wald.weinert@gmail.com>
2024-05-21 13:21:37 +0200

    Added a MiltipleSelect for worker and chnged the reusable comnponent Select in frontend

--- client/src/shared/UIElements/Select.js
+++ client/src/shared/UIElements/Select.js
@@ -1,5 +1,6 @@
-import { Dropdown } from 'react-native-element-dropdown';
-import { StyleSheet } from "react-native";
+import { Dropdown, MultiSelect } from 'react-native-element-dropdown';
+
+import { StyleSheet, View } from "react-native";
 import { useState, useEffect } from "react";
 
 import { validate } from "../../util/validators";
@@ -9,6 +10,7 @@ const Select = props => {
     const [isFocus, setIsFocus] = useState(false);
     const [isValid, setIsValid] = useState(true)
     const [value, setValue] = useState(null)
+    const [multiValue, setMultiValue] = useState([])
 
     const [selectedValue, setSelectedValue] = useState(props.initialSelectedValue); // Set initial selected value
 
@@ -18,9 +20,18 @@ const Select = props => {
         setIsValid(validate(item.value, props.validators));
         setSelectedValue(item.value)
         setValue(item.value)
+        // console.log(item)
     }
 
-    return <>
+    const multiHandleChange = (item) => {
+        props.onValueChange(item)
+        setIsValid(validate(item, props.validators));
+        setMultiValue(item)
+        // console.log(item)
+
+    }
+
+    const renderDropdown = () => (
         <Dropdown
             // placeholderStyle={styles.placeholderStyle}
             selectedTextStyle={styles.selectedTextStyle}
@@ -33,14 +44,38 @@ const Select = props => {
             labelField="label"
             valueField="value"
             placeholder={selectedValue ? selectedValue : 'Auswählen'}
-            visibleSelectedItem={true}
+            visibleSelectedItem={props.visibleSelectedItem || true}
             value={value}
             disable={props.disable}
             // onFocus={() => setIsFocus(true)}
             // onBlur={() => setIsFocus(false)}
             onChange={handleChange}
         />
-    </>
+    )
+
+    const renderMultiSelect = () => (
+        <MultiSelect
+            style={[props.disable && styles.disaabled, !isValid ? styles.selectInvalid : styles.select]}
+            inputSearchStyle={styles.inputSearchStyle}
+            selectedTextStyle={styles.selectedTextStyle}
+            iconStyle={styles.iconStyle}
+            search={props.search}
+            data={props.data}
+            labelField="label"
+            valueField="value"
+            placeholder={'Auswählen'}
+            value={multiValue}
+            onChange={multiHandleChange}
+            maxHeight={300}
+            disable={props.disable}
+            selectedStyle={styles.selectedStyle}
+            alwaysRenderSelectedItem={true}
+        // onFocus={() => setIsFocus(true)}
+        // onBlur={() => setIsFocus(false)}
+        />
+    )
+
+    return props.multi ? renderMultiSelect() : renderDropdown()
 }
 
 const styles = StyleSheet.create({
@@ -77,7 +112,10 @@ const styles = StyleSheet.create({
     },
     selectedTextStyle: {
         // color: 'red'
-    }
+    },
+    selectedStyle: {
+        borderRadius: 12,
+    },
 
 })
 
