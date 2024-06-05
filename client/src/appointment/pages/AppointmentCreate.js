cd15084460ee7fae0b1a082c6333c6fb71b356ab
Waldemar Weinert <wald.weinert@gmail.com>
2024-06-05 15:52:28 +0200

    Created new logic of the datetime piker using another npm package

--- client/src/appointment/pages/AppointmentCreate.js
+++ client/src/appointment/pages/AppointmentCreate.js
@@ -1,52 +1,22 @@
-import {
-    View,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    ActivityIndicator,
-    StyleSheet,
-    ScrollView
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { View, Text, StyleSheet } from "react-native";
 import { useContext, useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import axios from "axios";
-import DateTimePicker from '@react-native-community/datetimepicker';
 
-import { AuthContext } from "../../context/auth-context";
-import Select from "../../shared/UIElements/Select";
 import { VALIDATOR_SELECT, VALIDATOR_REQUIRE } from "../../util/validators";
+import Select from "../../shared/UIElements/Select";
 import Input from "../../shared/UIElements/Input";
 import Button from "../../shared/UIElements/Button";
 import { refershData } from "../../actions/utilActions";
 
 const AppointmentCreate = props => {
     const dispatch = useDispatch()
-    const auth = useContext(AuthContext)
-
-
     const firmId = useSelector(state => state.context.firmId)
 
-
     const [isLoaded, setIsLoaded] = useState(false)
-    const [date, setDate] = useState(new Date());
-    const [selectedDateOnly, setSelectedDateOnly] = useState('');
-    const [selectedTimeOnly, setSelectedTimeOnly] = useState('');
-
-    const onChange = (event, selectedDate) => {
-        const currentDate = selectedDate || date;
-        setDate(currentDate);
-
-
-        // Extract date and time components
-        const selectedDateTime = new Date(currentDate);
-        const dateOnly = selectedDateTime.toISOString().split('T')[0];
-        const timeOnly = selectedDateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-
-        setSelectedDateOnly(dateOnly);
-        setSelectedTimeOnly(timeOnly);
-    };
-
+    const [date, setDate] = useState(new Date(Date.now()));
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
     const [formData, setFormData] = useState({
         orderOptions: [],
         workerOptions: [],
@@ -112,8 +82,8 @@ const AppointmentCreate = props => {
                 orderId: formData.selectedOrder,
                 workers: formData.selectedWorkers,
                 description: formData.description,
-                date: selectedDateOnly,
-                time: selectedTimeOnly,
+                date: date.toISOString().split('T')[0],
+                time: date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
                 // status: status,
 
             });
@@ -127,24 +97,35 @@ const AppointmentCreate = props => {
         }
     };
 
+    // START: date picker logic
+    const showDatePicker = () => setDatePickerVisibility(true)
+    const hideDatePicker = () => setDatePickerVisibility(false)
+
+    const handleConfirm = (selectedDate) => {
+        const currentDate = new Date(date);
+        currentDate.setHours(selectedDate.getHours());
+        currentDate.setMinutes(selectedDate.getMinutes());
+        setDate(currentDate);
+        hideDatePicker();
+    };
+    // END: date picker logic 
 
     return isLoaded && (
         <View style={styles.container}>
 
             <Text style={styles.label}>Datum und Uhrzeit</Text>
 
-            <View style={styles.dataTimeContainer}>
-                <DateTimePicker
-                    testID="dateTimePicker"
-                    value={date}
-                    mode="datetime"
-                    is24Hour={true}
-                    display="default"
-                    onChange={onChange}
-                // style={styles.picker}
-                />
-
-            </View>
+            <Button
+                style={styles.pickerBtn}
+                onPress={showDatePicker}
+                title={`${date.toLocaleDateString()} ${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`}
+            />
+            <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="datetime"
+                onConfirm={handleConfirm}
+                onCancel={hideDatePicker}
+            />
 
             <Text style={styles.label}>Auftrag</Text>
 
@@ -171,14 +152,15 @@ const AppointmentCreate = props => {
 
 
             <Input
-                multiline={true}
-                numberOfLines={4}
-                textArea={true}
                 placeholder="Beschreibung"
                 value={formData.description}
                 validators={[VALIDATOR_REQUIRE()]}
                 errorText='Geben Sie die Beschreibung des Termins ein'
                 onChangeText={(text) => setFormData({ ...formData, description: text })}
+                style={[styles.textArea, styles.placeholderText]}
+                multiline={true}
+                numberOfLines={4}
+                textArea={true}
             />
 
             <View style={styles.btnContainer}>
@@ -268,19 +250,31 @@ const styles = StyleSheet.create({
     loader: {
         flex: 1,
     },
-    dataTimeContainer: {
-        // borderWidth: 2,
-        // borderColor: '#eee',
-        borderRadius: 5,
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        // paddingTop: 4,
-        // paddingBottom: 4
+    textArea: {
+        width: "100%",
+        height: 130, // Adjust the height as needed
+        borderColor: "#e0e0e0",
+        borderWidth: 1,
+        marginTop: 12,
+        paddingTop: 7,
+        paddingBottom: 7,
+        paddingLeft: 7,
+        paddingRight: 7,
+        fontSize: 18,
+        borderRadius: 6,
     },
-    picker: {
-        borderColor: "red",
-        borderWidth: 2,
-
+    placeholderText: {
+        color: "gray",
+        fontSize: 18, // Set the font size of the placeholder text
+    },
+    pickerBtn: {
+        borderColor: '#e0e0e0',
+        borderWidth: 1,
+        padding: 16,
+        borderRadius: 5,
+        justifyContent: "flex-start",
+        flexDirection: "row",
+        alignItems: "center",
     },
 });
 
